d6897c1c1b5679d84796adee96dd5839feb2151c
Alex <temper8@gmail.com>
2025-07-20 01:08:38 +0300

    Update index.py

--- pages/index.py
+++ pages/index.py
@@ -1,18 +1,17 @@
 import marimo
 
-__generated_with = "0.14.11"
-app = marimo.App(width="medium")
+__generated_with = "0.14.12"
+app = marimo.App(width="medium", app_title="Wave2D")
 
 
 @app.cell
 def _():
     import marimo as mo
-
     return (mo,)
 
 
 @app.cell
-def _(mo):
+def _():
     import ui_elements
     ui_elements.nav_menu()
     return
@@ -20,21 +19,31 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    mo.md("# Home. Test text")
+    mo.md(
+        """
+    ## Wave2D Box<br>
+    Это web-приложение написанное на marimo для просмотра реультатов вычисления Wave2D<br>
+    Удобное и интерактивное. Каждая страница по сути это блокнот Marimo. Его можно редактировать прямо в браузере
+
+    * **[Power](\power_absorbtion)** - раздел показывает поглощенную мощность по радиусу
+    * **Settings** - очевидно настройки
+    * **About** - еще не придумал о чем
+    """
+    )
     return
 
 
 @app.cell
 def _(mo):
     mo.md(
-        r'''
-        The exponential function $f(x) = e^x$ can be represented as
+        r"""
+    The exponential function $f(x) = e^x$ can be represented as
 
-        \[
-            f(x) = 1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + \ldots.
-        \]
-        '''
-        )
+    \[
+        f(x) = 1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + \ldots.
+    \]
+    """
+    )
     return
 
 
