89388ea7bb539aaeff31a4547b9a661440ccff5e
Alex <temper8@gmail.com>
2025-07-20 00:40:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,45 @@
 import marimo
 
-__generated_with = "0.13.6"
-app = marimo.App(width="medium")
+__generated_with = "0.14.11"
+app = marimo.App(width="medium", app_title="test")
+
+
+@app.cell
+def _(mo):
+    nav_menu = mo.nav_menu(
+        {
+            "/about": "About",
+            "/overview": "Overview",
+            "/sales": f"{mo.icon('lucide:shopping-cart')} Sales",
+            "/products": f"{mo.icon('lucide:package')} Products",
+        }
+    )
+    nav_menu
+    return
+
+
+@app.cell
+def _(mo):
+    def page1():
+        return mo.md("## This is page 1")
+    def render_about():
+        return mo.md("## This is page about")
+    def render_contact():
+        return mo.md("## This is page contact")        
+    return page1, render_about, render_contact
+
+
+@app.cell
+def _(mo, page1, render_about, render_contact):
+    mo.routes(
+        {
+            "#/": page1,
+            "#/about": render_about,
+            "#/contact": render_contact,
+            mo.routes.CATCH_ALL: page1,
+        }
+    )
+    return
 
 
 @app.cell
