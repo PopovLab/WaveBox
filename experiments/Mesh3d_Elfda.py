8c4c8ca3d3bc6b087ec3eb15cd685e298103b65a
Alex <temper8@gmail.com>
2025-09-25 01:23:19 +0300

    Create Mesh3d_Elfda.py

---
+++ experiments/Mesh3d_Elfda.py
@@ -0,0 +1,66 @@
+import marimo
+
+__generated_with = "0.15.5"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return
+
+
+@app.cell
+def _(df):
+    MarginRatio=1.1
+    Nr = 141
+    Nr_margin=int(Nr*MarginRatio)
+    N_teta = int(df.shape[0]/Nr_margin)
+    Nr, Nr_margin, N_teta
+    return
+
+
+@app.cell
+def _():
+    import pandas as pd
+    df = pd.read_table('Eflda_141.dat', header=None, names=['X','Y','eflda'], sep='\\s+')
+    min_eflda = df['eflda'].max()
+    return df, min_eflda
+
+
+@app.cell
+def _(df, min_eflda):
+    import plotly.graph_objects as go
+    import numpy as np
+
+    fig1 = go.Figure(data=[
+        go.Mesh3d(x=df['X'], y=df['Y'], z=df['eflda'], colorbar_x=1.07,
+                 intensity = df['eflda']/min_eflda, intensitymode='vertex', flatshading= False,)
+    ])
+
+    fig1.update_layout(
+        scene = dict(
+            aspectratio=dict(x=1, y=1, z=0.45),
+            xaxis = dict(nticks=4, range=[-10,10],),
+            yaxis = dict(nticks=4, range=[-10,10],),
+            zaxis = dict(nticks=4, range=[0,min_eflda],),),
+        width=700,
+        margin=dict(r=20, l=10, b=10, t=10))
+    fig1.show()
+    return
+
+
+@app.cell
+def _(df):
+    df
+    return
+
+
+@app.cell
+def _(df):
+    df.shape[0]
+    return
+
+
+if __name__ == "__main__":
+    app.run()
